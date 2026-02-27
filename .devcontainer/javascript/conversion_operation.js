0b7231c1b66698b21a1383e427147ffd33a32470
GOURAV0103 <en23cs301386@medicaps.ac.in>
2026-02-27 05:17:28 +0000

    conversion_operation

---
+++ .devcontainer/javascript/conversion_operation.js
@@ -0,0 +1,15 @@
+let score = "23asf";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+//"23" easily coverted into number 
+//"23asf" is NaN
+//"true" = 1 and "false" = 0
+
+let login = 1
+let bool = Boolean(login);
+console.log(bool);
