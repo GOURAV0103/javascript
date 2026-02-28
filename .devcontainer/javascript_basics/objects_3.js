cc69ae02766a6333862b55179923c87b7d09930b
GOURAV0103 <en23cs301386@medicaps.ac.in>
2026-02-28 16:54:11 +0000

    JSON

---
+++ .devcontainer/javascript_basics/objects_3.js
@@ -0,0 +1,27 @@
+const course = {
+    coursename: "javascript",
+    price:"999",
+    publishYear:2023,
+}
+
+
+const {price} = course
+const {coursename} = course
+
+// console.log(price);
+// console.log(coursename);
+//+++++++++++++++++++++++++++++API's+++++++++++++++++++++++
+//JSON
+// {
+//     "name":"sam",
+//     "coursename":"javacsript",
+//     "price":999
+
+// }
+
+
+
+
+
+
+
