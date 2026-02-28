61b1a076ca22c204546d1facbf41564c88f0a62b
GOURAV0103 <en23cs301386@medicaps.ac.in>
2026-02-28 19:26:27 +0000

    scopes_1

---
+++ .devcontainer/javascript_basics/scopes.js
@@ -0,0 +1,15 @@
+
+
+let a = 300
+//scope is defied by curlybrackets {}
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("inner a: ",a);
+    
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
